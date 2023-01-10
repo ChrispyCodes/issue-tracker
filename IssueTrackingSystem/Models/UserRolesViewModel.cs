0b43aad2860f2305fd9eeb91bbf93bb22732be01
Christopher <thetoph@outlook.com>
2023-01-10 15:32:22 -0800

    create model DTO for User-Role mangement UI

---
+++ IssueTrackingSystem/Models/UserRolesViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace IssueTrackingSystem.Models
+{
+    public class UserRolesViewModel
+    {
+        public string RoleName { get; set; }
+        public bool Selected { get; set; }
+    }
+    public class ManageUserRolesViewModel
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public List<UserRolesViewModel> UserRoles { get; set; }
+    }
+}
