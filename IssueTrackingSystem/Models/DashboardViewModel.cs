f3b8e203d9d2383e340ca12694c80949478acf9d
Christopher <thetoph@outlook.com>
2023-01-12 20:37:11 -0800

    dashboard changes

--- IssueTrackingSystem/Models/DashboardViewModel.cs
+++ IssueTrackingSystem/Models/DashboardViewModel.cs
@@ -2,6 +2,22 @@
 {
     public class DashboardViewModel
     {
+        public int OverdueIssues { get; set; }
+        public int OpenIssues { get; set; }
 
+        public int TotalIssues { get; set; }
+
+        public int TotalProjects { get; set; }
+
+        public int TotalUsers { get; set; }
+
+        public int AverageDaysToResolve { get; set; }
+        
+        
+
+        public IList<Issue> Issues { get; set; } = default!;
+
+
+        //AverageDaysToResolve = (int)Issues.Average(i => (i.ResolvedDate - i.CreatedDate).TotalDays);
     }
 }
